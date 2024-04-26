512112b3e83d185e88f5bbd15b861336c6179ec2
MXDMUSIC <149785373+MXDMUSIC@users.noreply.github.com>
2024-04-26 12:07:55 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/XDMUSIX/STRANGER-MUSIC",
+    "https://github.com/MXDMUSIC/DEVILMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
